e6dabc2b248f7ad89a6036610d37e094c48da896
chryosis <mdfox.au@gmail.com>
2017-11-05 17:56:57 -0600

    Changes

---
+++ js/ajaxUtils.js
@@ -0,0 +1,12 @@
+function loadDoc(chosenElement, chosenText) {
+var xhttp = new XMLHttpRequest();
+
+xhttp.onreadystatechange = function() {
+	if (this.readyState == 4 && this.status == 200) {
+ 		document.getElementById(chosenElement).innerHTML = this.responseText;
+	}
+};
+
+xhttp.open("GET", chosenText, true);
+xhttp.send();
+}
